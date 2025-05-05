e7c5d7f7ec0f4a85ea6a4f8f0e950a0126e1269a
Md Shadab Azam Ansari <34206533+sazamansari@users.noreply.github.com>
2025-05-05 21:04:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,7 @@ function App() {
 
   return (
    <div>
-    <h1>Welcome to React App with Impliment CI/CD Pipelines</h1>
-    <h2> with GITHUB ACTION</h2>
-    <h3>By Md Shadab Azam Ansari</h3>
-   <p> hello</p>
- <p>hello</p>
-<h1> new update website</h1>
-<h1> new update</h1>
-
-<h3>hello 2 g-22</h3>
+    <h1>hello my first CI CD PIPELINES ON REACT APP</h1>
    </div>
   )
 }
