353c7c5eaee1d735ebf164c0f53a941f186766be
shadab1995 <shadabazama@gmail.com>
2025-05-05 11:35:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div>
+    <h1>Welcome to React App with Impliment CI/CD Pipelines</h1>
+    <h2> with GITHUB ACTION</h2>
+    <h3>By Md Shadab Azam Ansari</h3>
+   </div>
+  )
+}
+
+export default App
